134c674447624b5977dbd55e0447eaf29d7abc68
Shaowei Song <songshaowei.ssw@antgroup.com>
2023-07-20 10:20:04 +0800

    Add layer 4-lsm and 5-disk

--- src/layers/3-log/mod.rs
+++ src/layers/3-log/mod.rs
@@ -4,6 +4,6 @@ pub mod chunk;
 pub mod raw_log;
 mod tx_log;
 
-pub use self::tx_log::{TxLog, TxLogStore};
+pub use self::tx_log::{TxLog, TxLogId, TxLogStore};
 
 // TODO: Use D as the parameter for BlockSet
