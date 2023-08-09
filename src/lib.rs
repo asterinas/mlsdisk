ee33d3d0a6e95d9f6e467346c163654d3d520d31
Shaowei Song <songshaowei.ssw@antgroup.com>
2023-08-09 15:31:40 +0800

    Workable transaction(tx) module

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 // #![no_std]
+#![feature(let_chains)]
+#![feature(negative_impls)]
 #![feature(new_uninit)]
 
 mod error;
 mod layers;
 mod os;
 mod prelude;
-// mod tx; // Uncomment this when it's ready
+mod tx;
 mod util;
 
 extern crate alloc;
