26f93feb8babb9d3d05e7f5b30a964244bbef5bc
Qingsong Chen <changxian.cqs@antgroup.com>
2023-08-15 20:40:52 +0800

    Add OS-specific abstractions

--- src/os/mod.rs
+++ src/os/mod.rs
@@ -1,3 +1,33 @@
 //! OS-specific or OS-dependent APIs.
 
-// thread_local
+#[cfg(feature = "jinux")]
+mod jinux;
+#[cfg(feature = "jinux")]
+pub use self::jinux::{
+    Aead, AeadIv, AeadKey, AeadMac, CurrentThread, Mutex, MutexGuard, Pages, Rng, Skcipher,
+    SkcipherIv, SkcipherKey, Tid, PAGE_SIZE,
+};
+
+#[cfg(feature = "linux")]
+mod linux;
+#[cfg(feature = "linux")]
+pub use self::linux::{
+    Aead, AeadIv, AeadKey, AeadMac, CurrentThread, Mutex, MutexGuard, Pages, Rng, Skcipher,
+    SkcipherIv, SkcipherKey, Tid, PAGE_SIZE,
+};
+
+#[cfg(all(feature = "occlum", target_env = "sgx"))]
+mod occlum;
+#[cfg(all(feature = "occlum", target_env = "sgx"))]
+pub use self::occlum::{
+    Aead, AeadIv, AeadKey, AeadMac, CurrentThread, Mutex, MutexGuard, Pages, Rng, Skcipher,
+    SkcipherIv, SkcipherKey, Tid, PAGE_SIZE,
+};
+
+#[cfg(feature = "std")]
+mod std;
+#[cfg(feature = "std")]
+pub use self::std::{
+    Aead, AeadIv, AeadKey, AeadMac, CurrentThread, Mutex, MutexGuard, Pages, Rng, Skcipher,
+    SkcipherIv, SkcipherKey, Tid, PAGE_SIZE,
+};
---
+++ src/os/occlum/mod.rs
@@ -0,0 +1,1 @@
+//! Occlum specific implementations.
