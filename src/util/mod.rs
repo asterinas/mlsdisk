bb2552c8538d461c3767969fd9ceade026d8646f
Qingsong Chen <changxian.cqs@antgroup.com>
2023-08-15 19:00:33 +0800

    Add crypto api

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,5 @@
 mod crypto;
 mod lazy_delete;
 
-pub use self::crypto::{Iv, Key, Mac};
+pub use self::crypto::{Aead, Iv, Key, Mac, RandomInit, Rng, Skcipher};
 pub use self::lazy_delete::LazyDelete;
