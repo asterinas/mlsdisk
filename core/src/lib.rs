85cab4e41a9747f612d8c546d97f9b4f2e8049b3
Shaowei Song <songshaowei.ssw@antgroup.com>
2024-02-29 11:33:39 +0800

    Add layer 5-disk

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,8 +1,11 @@
-// #![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 #![feature(allocator_api)]
+#![feature(anonymous_lifetime_in_impl_trait)]
 #![feature(coerce_unsized)]
 #![feature(dispatch_from_dyn)]
 #![feature(fn_traits)]
+#![feature(int_log)]
+#![feature(is_some_and)]
 #![feature(is_sorted)]
 #![feature(let_chains)]
 #![feature(negative_impls)]
@@ -21,4 +24,15 @@ mod prelude;
 mod tx;
 mod util;
 
+#[cfg(not(feature = "occlum"))]
 extern crate alloc;
+
+#[cfg(feature = "occlum")]
+#[macro_use]
+extern crate sgx_tstd;
+
+pub use self::error::{Errno, Error};
+pub use self::layers::bio::{BlockId, BlockSet, Buf, BufMut, BufRef, BLOCK_SIZE};
+pub use self::layers::disk::SwornDisk;
+pub use self::os::{Aead, AeadKey, Rng};
+pub use self::util::{Aead as _, RandomInit, Rng as _};
